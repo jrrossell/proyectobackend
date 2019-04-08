2c5ce8477f1bed47d1f7201bf256216b6bbcb384
MarinaFivi <fivi2793@gmail.com>
2019-04-08 11:51:17 +0200

    primer commit

---
+++ Multas/src/main/java/com/example/demo/MultasApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultasApplication.class, args);
+	}
+
+}
